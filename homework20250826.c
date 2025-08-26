7b682aed8fb8b962f483622fbf4e9083675a591a
tuneable <bendy.cha@kakao.com>
2025-08-26 20:42:17 +0900

    Function to identify the type of character

--- homework20250826.c
+++ homework20250826.c
@@ -1,22 +1,30 @@
 # include <stdio.h>
 # include <string.h>
 # include <ctype.h>
+char c;
+char str[20] = "";
+
+char separator(char ch) {
+    if(ch >= '0' && ch <= '9') {
+        strcpy(str, "숫자");
+    } else if (ch >= 'a' && ch <= 'z') {
+        strcpy(str, "소문자");
+    } else if (ch >= 'A' && ch <= 'Z') {
+        strcpy(str, "대문자");
+    } else if (ispunct(ch)) {
+        strcpy(str, "특수문자");
+    }
+    return str;
+}
+
+
 
 int main() {
     printf("한 글자를 입력하세요. ");
-    char c;
-    char str[20] = "";
+    
 
     scanf("%c", &c);
-    if(c >= '0' && c <= '9') {
-        strcpy(str, "숫자");
-    } else if (c >= 'a' && c <= 'z') {
-        strcpy(str, "소문자");
-    } else if (c >= 'A' && c <= 'Z') {
-        strcpy(str, "대문자");
-    } else if (ispunct(c)) {
-        strcpy(str, "특수문자");
-    }
+    separator(c);
     printf("입력하신 글자는 %s입니다.\n", str);
     return 0;
 }
